56388d469c7e49b368b9ce9b62987436fadf9ee5
19marius <prooms.email@gmail.com>
2026-06-03 23:10:59 +0300

    further deploy fixes

--- scripts/copy-404.js
+++
@@ -1,8 +0,0 @@
-import { copyFileSync, existsSync } from 'fs';
-
-copyFileSync('dist/index.html', 'dist/404.html');
-
-if (existsSync('public/resources/favicon.png'))
-{
-  copyFileSync('public/resources/favicon.png', 'dist/favicon.png');
-}
